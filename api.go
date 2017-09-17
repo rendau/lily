cda607089b005da31b9088f0660f6278f9505014
NDN <dauren.nurlanov@gmail.com>
2017-09-17 23:12:07 +0600

    add json funs

--- api.go
+++ api.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func ExtractPaginationPars(pars *url.Values) (offset uint64, limit uint64, page uint64) {
+func ApiExtractPaginationPars(pars *url.Values) (offset uint64, limit uint64, page uint64) {
 	var err error
 	qPar := pars.Get("page_size")
 	if qPar != "" {
@@ -32,7 +32,7 @@ func ExtractPaginationPars(pars *url.Values) (offset uint64, limit uint64, page 
 	return
 }
 
-func ExtractSortPars(pars *url.Values) (sortColumn string, sortDesc bool) {
+func ApiExtractSortPars(pars *url.Values) (sortColumn string, sortDesc bool) {
 	// TODO change this fun
 	sortColumn = pars.Get("sort_columns")
 	qPar := pars.Get("sort_order")
@@ -40,7 +40,25 @@ func ExtractSortPars(pars *url.Values) (sortColumn string, sortDesc bool) {
 	return
 }
 
-func PaginatedResponse(data string, page_size, page, total uint64) string {
+func ApiPaginatedResponse(data string, page_size, page, total uint64) string {
 	return fmt.Sprintf(`{"page_size":%d,"page":%d,"total_count":%d`, page_size, page, total) +
 		`,"results":` + data + `}`
+}
+
+func ApiJsonListQuery(q string) string {
+	return `(select coalesce(array_to_json(array_agg(row_to_json(sq.*))), '[]')
+		from (` + q + `) sq)`
+}
+
+func ApiJsonListStrQuery(q string) string {
+	return `select coalesce(array_to_json(array_agg(row_to_json(sq.*)))::text, '[]')
+		from (` + q + `) sq`
+}
+
+func ApiJsonRowQuery(q string) string {
+	return `(select row_to_json(sq.*) from (` + q + `) sq)`
+}
+
+func ApiJsonRowStrQuery(q string) string {
+	return `select row_to_json(sq.*)::text from (` + q + `) sq`
 }
