f9c1988e495f201470a1833f7c949c05b7018090
NDN <dauren.nurlanov@gmail.com>
2018-06-19 23:29:12 +0600

    some fixes

--- smsc.go
+++ smsc.go
@@ -40,16 +40,21 @@ var (
 	SmscDebug = false
 )
 
-func SmscSend(username, password string, phones string, msg string) bool {
+func SmscSend(username, password string, phones string, msg string) *SmscErrorSt {
 	if SmscDebug {
 		log.Printf("Sent sms: %s - %q\n", phones, msg)
-		return true
+		return nil
 	}
+
 	client := &http.Client{
 		Timeout: 20 * time.Second,
 	}
+
 	req, err := http.NewRequest("GET", urlPrefix+"send.php", nil)
-	ErrPanic(err)
+	if err != nil {
+		return &SmscErrorSt{Code: "request_create_error", Desc: "Fail to create new request - " + err.Error()}
+	}
+
 	params := req.URL.Query()
 	params.Add("login", username)
 	params.Add("psw", password)
@@ -57,35 +62,35 @@ func SmscSend(username, password string, phones string, msg string) bool {
 	params.Add("mes", msg)
 	params.Add("charset", "utf-8")
 	params.Add("fmt", "3")
+
 	req.URL.RawQuery = params.Encode()
+
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("(sms-send) fail to send sms:", err)
-		return false
+		return &SmscErrorSt{Code: "request_fail", Desc: "Fail to request smsc - " + err.Error()}
 	}
 	defer resp.Body.Close()
+
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("(sms-send) fail to read sms-send reply body:", err)
-		return false
+		return &SmscErrorSt{Code: "fail_to_read_body", Desc: "Fail to read body - " + err.Error()}
 	}
+
 	if resp.StatusCode != 200 {
-		log.Printf("(sms-send) bad status code %d in sms-send reply, data: %s\n", resp.StatusCode, string(data))
-		return false
+		return &SmscErrorSt{Code: "bad_status_code", Desc: "Bad status code - " + strconv.Itoa(resp.StatusCode) + ", body - " + string(data)}
 	}
+
 	reply := smscSendReplySt{}
 	err = json.Unmarshal(data, &reply)
 	if err != nil {
-		log.Printf("(sms-send) fail to parse sms-send reply: %s, %s\n", err.Error(), string(data))
-		return false
+		return &SmscErrorSt{Code: "fail_to_parse_body", Desc: "Fail to parse body - " + err.Error()}
 	}
+
 	if (reply.ErrorCode != 0) || (reply.Error != "") {
-		if reply.ErrorCode != 7 && reply.ErrorCode != 8 { // 7 - invalid number, 8 - can't to deliver
-			log.Printf("(sms-send) sms provider error for (%s, %q):\n%s\n", phones, msg, string(data))
-		}
-		return false
+		return &SmscErrorSt{Code: "provider_error_" + strconv.Itoa(reply.ErrorCode), Desc: "Provider error for message '" + msg + "' - " + string(data)}
 	}
-	return true
+
+	return nil
 }
 
 func SmscSendBcast(username, password string, phones string, msg string) (*SmscErrorSt, uint64) {
@@ -100,7 +105,7 @@ func SmscSendBcast(username, password string, phones string, msg string) (*SmscE
 
 	req, err := http.NewRequest("GET", urlPrefix+"jobs.php", nil)
 	if err != nil {
-		return &SmscErrorSt{Code: "request_fail", Desc: "Fail to create new request - " + err.Error()}, 0
+		return &SmscErrorSt{Code: "request_create_error", Desc: "Fail to create new request - " + err.Error()}, 0
 	}
 
 	params := req.URL.Query()
@@ -137,7 +142,7 @@ func SmscSendBcast(username, password string, phones string, msg string) (*SmscE
 	}
 
 	if (reply.ErrorCode != 0) || (reply.Error != "") {
-		return &SmscErrorSt{Code: "provider_error", Desc: "Provider error for message '" + msg + "' - " + string(data)}, 0
+		return &SmscErrorSt{Code: "provider_error_" + strconv.Itoa(reply.ErrorCode), Desc: "Provider error for message '" + msg + "' - " + string(data)}, 0
 	}
 
 	return nil, reply.ID
@@ -152,7 +157,7 @@ func SmscGetBalance(username, password string) (*SmscErrorSt, float64) {
 
 	req, err := http.NewRequest("GET", urlPrefix+"balance.php", nil)
 	if err != nil {
-		return &SmscErrorSt{Code: "request_fail", Desc: "Fail to create new request - " + err.Error()}, 0
+		return &SmscErrorSt{Code: "request_create_error", Desc: "Fail to create new request - " + err.Error()}, 0
 	}
 
 	params := req.URL.Query()
@@ -184,7 +189,7 @@ func SmscGetBalance(username, password string) (*SmscErrorSt, float64) {
 	}
 
 	if (reply.ErrorCode != 0) || (reply.Error != "") {
-		return &SmscErrorSt{Code: "provider_error", Desc: "Provider error - " + string(data)}, 0
+		return &SmscErrorSt{Code: "provider_error_" + strconv.Itoa(reply.ErrorCode), Desc: "Provider error - " + string(data)}, 0
 	}
 
 	result, _ = strconv.ParseFloat(reply.Balance, 64)
